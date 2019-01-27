f03f2739a73630b121a3004e590f02a28576633f
Артем <39417316+Ste11@users.noreply.github.com>
2019-01-27 20:11:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-var port = process.env.PORT || 3000; 
-app.listen (порт, "0.0.0.0", function () { 
-console.log ("Listening on Port 3000"); 
-}); 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 const Discord = require('discord.js');
 const Bot = new Discord.Client();
 Bot.commands = new Discord.Collection();
