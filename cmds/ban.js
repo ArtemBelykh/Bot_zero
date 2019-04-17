1f154771eba655a045ca3c7bc25c65ad7bf1c1e5
{name} <{emailaddress}>
2019-04-17 20:41:57 +0300

    first commit

--- cmds/ban.js
+++ cmds/ban.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 let profile = require("../profile.json");
 module.exports.run = async (Bot,message,args) => {
     try{
-      
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("У вас нет прав");
-    let rUser = Bot.rUser;
+
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("У вас нет прав, на использование данной команды!");
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!args[0]) return Bot.send("Вы не указали пользователя");
     if(!rUser) return Bot.send("Пользователь не найден");
     let embed = new Discord.RichEmbed()
